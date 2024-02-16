7af8e1324159541993673eb65ec593be7a6f548b
danilovalerio <danilo_vs@hotmail.com>
2024-02-16 15:16:32 -0300

    Repositories fix

--- src/main/kotlin/com/danilo/project/taskmanager/taskmanager/core/repositories/TaskRepository.kt
+++ src/main/kotlin/com/danilo/project/taskmanager/taskmanager/core/repositories/TaskRepository.kt
@@ -3,5 +3,5 @@ package com.danilo.project.taskmanager.taskmanager.core.repositories
 import com.danilo.project.taskmanager.taskmanager.core.models.Task
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TaskRepository : JpaRepository<Long, Task> {
+interface TaskRepository : JpaRepository<Task, Long> {
 }
--- src/main/kotlin/com/danilo/project/taskmanager/taskmanager/core/repositories/UsuarioRepository.kt
+++ src/main/kotlin/com/danilo/project/taskmanager/taskmanager/core/repositories/UsuarioRepository.kt
@@ -3,5 +3,5 @@ package com.danilo.project.taskmanager.taskmanager.core.repositories
 import com.danilo.project.taskmanager.taskmanager.core.models.Usuario
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UsuarioRepository : JpaRepository<Long, Usuario> {
+interface UsuarioRepository : JpaRepository<Usuario, Long> {
 }

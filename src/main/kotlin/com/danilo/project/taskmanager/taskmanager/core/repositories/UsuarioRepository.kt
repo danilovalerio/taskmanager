ba7dbc268937a9426801b5462db019d170d88635
danilovalerio <danilo_vs@hotmail.com>
2024-02-20 17:29:43 -0300

    Query Creation Spring Data JPA - Para buscar por email

--- src/main/kotlin/com/danilo/project/taskmanager/taskmanager/core/repositories/UsuarioRepository.kt
+++ src/main/kotlin/com/danilo/project/taskmanager/taskmanager/core/repositories/UsuarioRepository.kt
@@ -2,6 +2,9 @@ package com.danilo.project.taskmanager.taskmanager.core.repositories
 
 import com.danilo.project.taskmanager.taskmanager.core.models.Usuario
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface UsuarioRepository : JpaRepository<Usuario, Long> {
+
+    fun findByEmail(email: String): Optional<Usuario>
 }

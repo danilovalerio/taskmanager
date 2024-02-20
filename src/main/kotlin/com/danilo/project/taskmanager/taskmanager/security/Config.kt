4bbea1ddaf3856ffd002240e432b932712a826d1
danilovalerio <danilo_vs@hotmail.com>
2024-02-20 16:38:50 -0300

    Config Inicial - liberando acesso a todas a rotas

---
+++ src/main/kotlin/com/danilo/project/taskmanager/taskmanager/security/Config.kt
@@ -0,0 +1,22 @@
+package com.danilo.project.taskmanager.taskmanager.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import kotlin.jvm.Throws
+
+@Configuration
+@EnableWebMvc
+class Config {
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        httpSecurity.authorizeHttpRequests {auth ->
+            auth.anyRequest().permitAll()
+
+        }
+        return httpSecurity.build()
+    }
+}

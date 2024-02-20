8bca3b2d3e529f32d21204ad84da119ae2ee87b3
danilovalerio <danilo_vs@hotmail.com>
2024-02-20 17:43:36 -0300

    Autenticação - Model e Service

---
+++ src/main/kotlin/com/danilo/project/taskmanager/taskmanager/auth/AutenticacaoService.kt
@@ -0,0 +1,23 @@
+package com.danilo.project.taskmanager.taskmanager.auth
+
+import com.danilo.project.taskmanager.taskmanager.core.repositories.UsuarioRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+
+class AutenticacaoService : UserDetailsService {
+
+    @Autowired
+    private lateinit var repository: UsuarioRepository
+
+    override fun loadUserByUsername(email: String?): UserDetails {
+        val msg = "Erro ao tentar conectar com usuário $email, entre em contato com o responsável."
+
+        return repository.findByEmail(email ?: "").map { user ->
+            UsuarioAutenticado(user)
+        }.orElseThrow {
+            //TODO: Tratar com Exception Personalizada
+            RuntimeException(msg)
+        }
+    }
+}
---
+++ src/main/kotlin/com/danilo/project/taskmanager/taskmanager/auth/UsuarioAutenticado.kt
@@ -0,0 +1,38 @@
+package com.danilo.project.taskmanager.taskmanager.auth
+
+import com.danilo.project.taskmanager.taskmanager.core.models.Usuario
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.userdetails.UserDetails
+
+class UsuarioAutenticado(user: Usuario) : UserDetails {
+
+    private var user: Usuario = user
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return  AuthorityUtils.createAuthorityList(user.tipo.toString())
+    }
+
+    override fun getPassword(): String {
+        return user.senha ?: ""
+    }
+
+    override fun getUsername(): String {
+        return user.email ?: ""
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+}

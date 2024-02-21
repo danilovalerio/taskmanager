9208c5fca900d7cc8a85a1c94b927475978580d7
danilovalerio <danilo_vs@hotmail.com>
2024-02-21 17:52:29 -0300

    Rename format

--- src/main/kotlin/com/danilo/project/taskmanager/taskmanager/api/utils/FormatUtils.kt
+++ src/main/kotlin/com/danilo/project/taskmanager/taskmanager/api/utils/FormatUtils.kt
@@ -3,7 +3,7 @@ package com.danilo.project.taskmanager.taskmanager.api.utils
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-object Format {
+object FormatUtils {
 
     fun dateFormatError(localDateTime: LocalDateTime): String {
         return localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)

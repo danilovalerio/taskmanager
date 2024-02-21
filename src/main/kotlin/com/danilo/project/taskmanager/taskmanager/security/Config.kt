50139671022520ded6cc117f2cbea19b0bde936a
danilovalerio <danilo_vs@hotmail.com>
2024-02-21 17:05:30 -0300

    /api/usuarios - disponível somente para adms

--- src/main/kotlin/com/danilo/project/taskmanager/taskmanager/security/Config.kt
+++ src/main/kotlin/com/danilo/project/taskmanager/taskmanager/security/Config.kt
@@ -1,5 +1,9 @@
 package com.danilo.project.taskmanager.taskmanager.security
 
+import com.danilo.project.taskmanager.taskmanager.api.handlers.TokenAccessDeniedHandler
+import com.danilo.project.taskmanager.taskmanager.api.handlers.TokenAuthenticationEntryPoint
+import com.danilo.project.taskmanager.taskmanager.core.enums.TipoUsuario
+import com.danilo.project.taskmanager.taskmanager.core.filters.AccessTokenRequestFilter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,6 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
@@ -24,6 +29,18 @@ class Config {
 
     @Autowired
     private lateinit var passwordEncoder: PasswordEncoder
+
+    /**
+     * Filter criado para autorização via token para nossa api
+     */
+    @Autowired
+    private lateinit var accessTokenRequestFilter: AccessTokenRequestFilter
+
+    @Autowired
+    private lateinit var tokenAuthenticationEntryPoint: TokenAuthenticationEntryPoint
+
+    @Autowired
+    private lateinit var tokenAccessDeniedHandler: TokenAccessDeniedHandler
 
     @Autowired
     @Throws(java.lang.Exception::class)
@@ -42,6 +59,9 @@ class Config {
     fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
         httpSecurity.authorizeHttpRequests {auth ->
             auth.requestMatchers(AntPathRequestMatcher("/api/auth/**")).permitAll()
+            auth.requestMatchers(AntPathRequestMatcher("/api/usuarios")).hasAuthority(
+                TipoUsuario.ADMINISTRADOR.name
+            )
             //auth.anyRequest().authenticated()
             //auth.anyRequest().permitAll()
         }
@@ -53,6 +73,22 @@ class Config {
         httpSecurity.csrf()
             .ignoringRequestMatchers("/api/**")
             .ignoringRequestMatchers("/auth/**")
+
+        /**
+         * accessTokenRequestFilter virá antes do filtro de autenticação padrão do Spring (UsernamePasswordAuthenticationFilter)
+         */
+        httpSecurity
+            .addFilterBefore(accessTokenRequestFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        /**
+         * authenticationEntryPoint: Trata exceções de acessar Rota sem estar autenticado (sem token ou token inválido)
+         * accessDeniedHandler: Quando o usuário não tem acesso a rota
+         */
+        httpSecurity.exceptionHandling { exceptionHandlingCustomizer ->
+            exceptionHandlingCustomizer
+                .authenticationEntryPoint(tokenAuthenticationEntryPoint)
+                .accessDeniedHandler(tokenAccessDeniedHandler)
+        }
 
         return httpSecurity.build()
     }

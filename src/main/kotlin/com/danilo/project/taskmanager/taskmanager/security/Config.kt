d68c9cd84e5d77c7e4dfe4251f3e9a7ff575303d
danilovalerio <danilo_vs@hotmail.com>
2024-02-21 14:27:35 -0300

    Beans - AuthenticationManager e UserDetailsService

--- src/main/kotlin/com/danilo/project/taskmanager/taskmanager/security/Config.kt
+++ src/main/kotlin/com/danilo/project/taskmanager/taskmanager/security/Config.kt
@@ -1,15 +1,42 @@
 package com.danilo.project.taskmanager.taskmanager.security
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import kotlin.jvm.Throws
 
 @Configuration
 @EnableWebMvc
+@EnableMethodSecurity(prePostEnabled = true)
 class Config {
+
+    @Autowired
+    private lateinit var userDetailsService: UserDetailsService
+
+    @Autowired
+    private lateinit var passwordEncoder: PasswordEncoder
+
+    @Autowired
+    @Throws(java.lang.Exception::class)
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder)
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration?): AuthenticationManager {
+        return authenticationConfiguration!!.getAuthenticationManager()
+    }
+
     @Bean
     @Throws(Exception::class)
     fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
